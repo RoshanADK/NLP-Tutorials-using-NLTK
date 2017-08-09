f34818ba3105aff04cace909a9076960d05bfffd
RoshanADK <roshannadhikari039@gmail.com>
2017-08-09 22:42:18 +0530

    v1

---
+++ requirements.py
@@ -0,0 +1,10 @@
+
+
+Before starting the Journey , we need few things.
+
+Install python (any version. Mine was 3.5)
+Install PIP installer (If you dont know PING me at "omnityblog.weebly.com")
+PIP install nltk (Or download and install , preferably PIP installed)
+
+That's all we are good to go.
+If you have any doubts feel free to ping me at "omnityblog.weebly.com"
