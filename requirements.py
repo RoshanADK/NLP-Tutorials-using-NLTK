07d262996543089c2eb7ccff5da21fe7473efff5
Roshan Adhikari <roshannadhikari039@gmail.com>
2017-08-09 22:44:28 +0530

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -3,8 +3,8 @@
 Before starting the Journey , we need few things.
 
 Install python (any version. Mine was 3.5)
-Install PIP installer (If you dont know PING me at "omnityblog.weebly.com")
-PIP install nltk (Or download and install , preferably PIP installed)
+Install PIP installer (If you don't know how to install pip PING me at "omnityblog.weebly.com"  or else there is a thread in stack overflow)
+PIP install nltk (Or download and install , preferably PIP install)
 
 That's all we are good to go.
 If you have any doubts feel free to ping me at "omnityblog.weebly.com"
